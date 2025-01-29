347fe4974b982a88e86b840ec7b38cfb39d38463
이민이 <mingramer@gmail.com>
2025-01-29 09:42:55 +0900

    네트워크 예제에 예외처리 입히기

---
+++ src/exception/ex2/MainV2.java
@@ -0,0 +1,21 @@
+package exception.ex2;
+
+import java.util.Scanner;
+
+public class MainV2 {
+    public static void main(String[] args) throws NetworkClientExceptionV2 {
+        NetworkServiceV2_4 service = new NetworkServiceV2_4();
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
+            System.out.print("전송할 문자: ");
+            String input = scanner.nextLine();
+            if(input.equals("exit")) {
+                break;
+            }
+            service.sendMessage(input);
+            System.out.println();
+        }
+        System.out.println("프로그램을 정상 종료 합니다.");
+    }
+}
