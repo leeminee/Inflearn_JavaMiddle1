101c93834615caeed03b02b0c60ea585e40627bb
이민이 <mingramer@gmail.com>
2025-01-29 10:33:44 +0900

    예외 계층화 하기

---
+++ src/exception/ex3/exception/MainV3.java
@@ -0,0 +1,21 @@
+package exception.ex3.exception;
+
+import java.util.Scanner;
+
+public class MainV3 {
+    public static void main(String[] args) {
+        NetworkServiceV3_2 service = new NetworkServiceV3_2();
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
+            System.out.print("전송할 문자: ");
+            String input = scanner.nextLine();
+            if(input.equals("exit")) {
+                break;
+            }
+            service.sendMessage(input);
+            System.out.println();
+        }
+        System.out.println("프로그램을 정상 종료 합니다.");
+    }
+}
